f927489fe7699b71751158327bad9ce08ee3a740
devcodemagic <129328691+devcodemagic@users.noreply.github.com>
2023-03-31 01:01:31 +0700

    Update index.js

--- locales/index.js
+++ locales/index.js
@@ -2,11 +2,13 @@ import config from '../config/index.js';
 import en from './en.js';
 import ja from './ja.js';
 import zh from './zh.js';
+import th from './th.js';
 
 const locales = {
   en,
   ja,
   zh,
+  th,
 };
 
 const t = (key) => locales[config.APP_LANG][key];
